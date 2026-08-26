a48f38c00f4b1cf3af9f8fdddd6cc84ff3d4e134
Fusumi Vietnam <fusumivietnam@gmail.com>
2026-08-26 16:52:07 +0700

    refactor: remove retired PowerShell Docker wrapper

--- scripts/docker-dev-down.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$Root = Split-Path -Parent $PSScriptRoot
-& powershell -NoProfile -ExecutionPolicy Bypass -File (Join-Path $Root 'scripts/songchart.ps1') dev down
-exit $LASTEXITCODE
