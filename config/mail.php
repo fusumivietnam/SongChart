b11cb3a4835cde210c26ab89f4c677c43646e733
Fusumi Vietnam <fusumivietnam@gmail.com>
2026-09-02 18:35:22 +0700

    chore: converge mail brand to SongChart

--- config/mail.php
+++ config/mail.php
@@ -9,5 +9,5 @@ return [
         'log' => ['transport' => 'log', 'channel' => env('MAIL_LOG_CHANNEL')],
         'array' => ['transport' => 'array'],
     ],
-    'from' => ['address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'), 'name' => env('MAIL_FROM_NAME', 'SongChartWeb')],
+    'from' => ['address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'), 'name' => env('MAIL_FROM_NAME', 'SongChart')],
 ];
