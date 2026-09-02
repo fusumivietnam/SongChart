1fc9b2f20da13a4b95a9e0b3b020fc3a3e2a3279
Fusumi Vietnam <fusumivietnam@gmail.com>
2026-09-02 18:34:37 +0700

    chore: converge runtime brand to SongChart

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'name' => env('APP_NAME', 'SongChartWeb'),
+    'name' => env('APP_NAME', 'SongChart'),
     'version' => env('APP_VERSION', '1.0.0'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
