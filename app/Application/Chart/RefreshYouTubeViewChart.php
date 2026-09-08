59912878ebb1fc1343279bf42ae44b20c7f2a31b
Fusumi Vietnam <fusumivietnam@gmail.com>
2026-09-09 01:04:46 +0700

    feat: connect YouTube metric observations to persisted chart snapshots

---
+++ app/Application/Chart/RefreshYouTubeViewChart.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Chart;
+
+use App\Domain\Chart\DTO\ChartSnapshot;
+use App\Support\Chart\DatabaseChartSnapshotStore;
+use App\Support\Chart\YouTubeViewCountObservationSource;
+use DateTimeImmutable;
+use DateTimeZone;
+
+final readonly class RefreshYouTubeViewChart
+{
+    public const CHART_ID = 'youtube-video-views';
+
+    public function __construct(
+        private YouTubeViewCountObservationSource $source,
+        private DatabaseChartSnapshotStore $store,
+    ) {}
+
+    public function handle(): ?string
+    {
+        $observations = $this->source->fetchApproved();
+        if ($observations === []) {
+            return null;
+        }
+
+        $snapshotAt = new DateTimeImmutable('now', new DateTimeZone('UTC'));
+        $snapshot = (new BuildChartSnapshot)->handle(
+            self::CHART_ID,
+            YouTubeViewCountObservationSource::METRIC,
+            $snapshotAt,
+            $observations,
+        );
+
+        return $this->store->append($snapshot);
+    }
+}
