e15f79e9486cf0a85b2311ba2d146c95f886ae58
Fusumi Vietnam <fusumivietnam@gmail.com>
2026-09-09 01:18:38 +0700

    style: normalize Stage 22 chart closure

--- tests/Feature/Chart/YouTubeViewChartFlowTest.php
+++ tests/Feature/Chart/YouTubeViewChartFlowTest.php
@@ -11,7 +11,6 @@ use App\Models\ProviderDestination;
 use App\Support\Chart\DatabaseChartSnapshotStore;
 use App\Support\Chart\YouTubeViewCountObservationSource;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
 
 uses(RefreshDatabase::class);
