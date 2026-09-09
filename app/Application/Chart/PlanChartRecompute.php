6167ce5662d0c2b5d6f1d4f6349707b23f284eff
Fusumi Vietnam <fusumivietnam@gmail.com>
2026-09-09 08:24:59 +0700

    feat: add bounded chart recompute planner

---
+++ app/Application/Chart/PlanChartRecompute.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Chart;
+
+use App\Domain\Catalog\Enums\EntityType;
+use App\Support\Chart\ChartDefinitionRegistry;
+
+final readonly class PlanChartRecompute
+{
+    public function __construct(private ChartDefinitionRegistry $definitions) {}
+
+    /** @return list<string> */
+    public function forCanonicalChange(EntityType $entityType): array
+    {
+        return array_values(array_map(
+            static fn (array $definition): string => (string) $definition['id'],
+            $this->definitions->activeForEntityType($entityType),
+        ));
+    }
+}
